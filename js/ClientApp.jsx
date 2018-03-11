3efb7d4fbf2ec02512f403f7ec35da569c8440e0
Jeroen De Swaef <jeroen@faction.xyz>
2018-03-11 16:47:28 +0100

    Rendering bubbles of cards

--- js/ClientApp.jsx
+++ js/ClientApp.jsx
@@ -1,12 +1,21 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { shape, string, arrayOf } from 'prop-types';
 
 import '../styles/main.scss';
 
-const Trainer = function trainerComponent() {
+import cards from '../cards.json';
+
+const Trainer = function trainerComponent(props) {
   return (
     <div className="trainer">
-      <div className="trainer__conversation" />
+      <div className="trainer__conversation">
+        <ol>
+          {props.cards.map(card => (
+            <li key={card.uuid} className="speech-bubble"><span>{card.answer.content}</span></li>
+          ))}
+        </ol>
+      </div>
       <div className="trainer__action-bar">
         <input type="text" placeholder="your answer" />
         <button className="button">Answer</button>
@@ -15,4 +24,23 @@ const Trainer = function trainerComponent() {
   );
 };
 
-render(<Trainer />, document.getElementById('app'));
+Trainer.propTypes = shape({
+  cards: arrayOf(
+    shape({
+      answer: shape({
+        content: string.isRequired,
+        contentType: string.isRequired,
+        gender: string.isRequired,
+        grammarType: string.isRequired,
+        language: string.isRequired
+      }).isRequired,
+      question: shape({
+        content: string.isRequired,
+        language: string.isRequired
+      }).isRequired,
+      uuid: string.isRequired
+    }).isRequired
+  ).isRequired
+}).isRequired;
+
+render(<Trainer cards={cards} />, document.getElementById('app'));
