bf561e314bf50f26f627d58d30bde920bbe2b998
Jeroen De Swaef <jeroen@faction.xyz>
2018-03-11 14:59:46 +0100

    Integrated milligram

--- js/ClientApp.jsx
+++ js/ClientApp.jsx
@@ -4,7 +4,15 @@ import { render } from 'react-dom';
 import '../styles/main.scss';
 
 const Trainer = function trainerComponent() {
-  return <div className="trainer" />;
+  return (
+    <div className="trainer">
+      <div className="trainer__conversation" />
+      <div className="trainer__action-bar">
+        <input type="text" placeholder="your answer" />
+        <button className="button">Answer</button>
+      </div>
+    </div>
+  );
 };
 
 render(<Trainer />, document.getElementById('app'));
