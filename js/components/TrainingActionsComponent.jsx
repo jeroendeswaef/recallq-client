6ff17c31f9edc2fbeb6f7f7769482b8f8c773579
Jeroen De Swaef <jeroen@faction.xyz>
2018-03-12 19:40:53 +0100

    Fix the enter button

--- js/components/TrainingActionsComponent.jsx
+++ js/components/TrainingActionsComponent.jsx
@@ -4,32 +4,32 @@ import { shape, func } from 'prop-types';
 class TrainingActionsComponent extends React.PureComponent {
   constructor(props) {
     super(props);
-    this.handleClick = this.handleClick.bind(this);
-    this.changeEnteredAnswer = this.changeEnteredAnswer.bind(this);
     this.state = {
       enteredAnswer: '',
       answer: props.answer
     };
   }
-  handleClick() {
+  handleAnswer = event => {
+    // To prevent the submit from submitting the page
+    event.preventDefault();
     this.state.answer(this.state.enteredAnswer);
     this.setState({ enteredAnswer: '' });
-  }
-  changeEnteredAnswer(event) {
+  };
+  changeEnteredAnswer = event => {
     this.setState({ enteredAnswer: event.currentTarget.value });
-  }
+  };
 
   render() {
     return (
-      <div className="trainer__action-bar">
+      <form onSubmit={this.handleAnswer} className="trainer__action-bar">
         <input
           value={this.state.enteredAnswer}
           onChange={this.changeEnteredAnswer}
           type="text"
           placeholder="your answer"
         />
-        <button className="button" onClick={this.handleClick}>Answer</button>
-      </div>
+        <button type="submit" className="button">Answer</button>
+      </form>
     );
   }
 }
