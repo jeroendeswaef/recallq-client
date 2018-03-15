9dd007e4b3d44162b337536c1a7791890ceed3b0
Jeroen De Swaef <jeroen@faction.xyz>
2018-03-15 13:17:49 +0100

    Clicking special character buttons adds to input

--- js/components/TrainingActionsComponent.jsx
+++ js/components/TrainingActionsComponent.jsx
@@ -21,14 +21,32 @@ interface TrainingActionsProps {
     this.input.focus();
   };
 
+  handleSpecialCharacter = (event: Event) => {
+    if (!(event.currentTarget instanceof HTMLButtonElement)) throw new Error('Expected button for special character');
+    const buttonElement: HTMLButtonElement = event.currentTarget;
+    const specialCharacter = buttonElement.getAttribute('data-character');
+    if (specialCharacter) {
+      this.input.value = `${this.input.value}${specialCharacter}`;
+      this.input.focus();
+    }
+  };
+
   render() {
-    console.info('TAC rerender');
     return (
-      <div>
-        {this.props.upcomingSpecialCharacters
-          .sort()
-          .map(character => <button key={character} className="button button-outline">{character}</button>)}
-        <form onSubmit={this.handleAnswer} className="trainer__action-bar">
+      <div className="trainer__action-bar">
+        <div className="trainer__action-bar__characters">
+          {this.props.upcomingSpecialCharacters.sort().map(character => (
+            <button
+              key={character}
+              onClick={this.handleSpecialCharacter}
+              data-character={character}
+              className="button button-outline"
+            >
+              {character}
+            </button>
+          ))}
+        </div>
+        <form onSubmit={this.handleAnswer} className="trainer__action-bar__message-actions">
           <input
             type="text"
             placeholder="your answer"
